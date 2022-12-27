ea5c37cba0bf10b84882f4ddd297509f19452211
sssajjad007 <sssajjad.76@gmail.com>
2022-12-27 13:01:05 +0330

    add all pacakges and dep + font + some config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 120,
+};
